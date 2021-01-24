f11da11b746b125ae292c818fbf97ed5206739fb
nish.gowda6@gmail.com <60044986+nishgowda@users.noreply.github.com>
2021-01-24 11:36:14 -0500

    comment and readme refactors

--- detect.py
+++ detect.py
@@ -16,6 +16,8 @@ model = CNN()
 model.load_state_dict(torch.load(f"models/{model_name}"))
 model.eval()
 
+## We use this alphabet dictionary to check the output more easily since we can just see 
+## the actual letter instead of the numeric label representation
 signs = {'0': 'A', '1': 'B', '2': 'C', '3': 'D', '4': 'E', '5': 'F', '6': 'G', '7': 'H', '8': 'I',
         '10': 'K', '11': 'L', '12': 'M', '13': 'N', '14': 'O', '15': 'P', '16': 'Q', '17': 'R',
         '18': 'S', '19': 'T', '20': 'U', '21': 'V', '22': 'W', '23': 'X', '24': 'Y' }
--- train.py
+++ train.py
@@ -20,6 +20,8 @@ from torchvision.utils import save_image
 import sys
 from model import CNN
 
+## We use this alphabet dictionary to check the output more easily since we can just see 
+## the actual letter instead of the numeric label representation
 signs = {'0': 'A', '1': 'B', '2': 'C', '3': 'D', '4': 'E', '5': 'F', 
          '6': 'G', '7': 'H', '8': 'I', '10': 'K', '11': 'L', '12': 'M', 
          '13': 'N', '14': 'O', '15': 'P', '16': 'Q', '17': 'R', '18': 'S', 
@@ -30,7 +32,7 @@ class SignsLanguageDataset(Dataset):
         self.train = train
         self.transform = transform
         
-        # uses standard mnist sign language translation testing and training data
+        # uses the kaggle mnist sign language translation testing and training data
         if self.train == True:
             self.signs_lang_dataset = pd.read_csv('data/input/sign_mnist_train.csv')
         else:
@@ -76,7 +78,6 @@ def train(model, optimizer, epoch, device, train_loader, log_interval):
 
 def test(model, device, test_loader):
     model.eval()
-    
     test_loss = 0
     correct = 0
 
@@ -112,9 +113,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 learning_rate = 0.001
 num_epochs = 7
 
+# Train and test our network with the MNIST Sign Language Dataset
 model = CNN()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.2, weight_decay=0.002)
-
 log_interval = 27455
 
 for epoch in range(1, num_epochs + 1):
@@ -122,7 +123,6 @@ for epoch in range(1, num_epochs + 1):
     test(model, device, test_loader)
 
 model_name = sys.argv[1]
-
 model_path = f"models/{model_name}"
 torch.save(model.state_dict(), model_path)
 print(f"Saved model to {model_path}")
